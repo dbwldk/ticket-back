6f3dc46a3e6ca1deac28510c731176b3bfb15674
dbwldk <yujiayujia55@naver.com>
2024-05-24 23:06:27 +0900

    도메인 수정

--- Project/TOW_Backend/src/main/java/com/tow/domain/TicketDB.java
+++ Project/TOW_Backend/src/main/java/com/tow/domain/TicketDB.java
@@ -10,17 +10,20 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "ticket")
+@Table(name = "tickets")
 public class TicketDB {
 	@Id
-	private Long ticket_num;
-	private String category;
-	private String title;
-	private Date register_date;
+	private Integer id;
+	private String event_name;
+	private Date registration_date;
+	private Timestamp ticket_open_date;
 	private Timestamp pre_sale_date;
-	private Timestamp open_sale_date;
 	private String image_url;
 	private String basic_info;
-	private String introduction;
+	private String event_description;
 	private String agency_info;
+	private String detail_link;
+	private String genre;
+	private String sales_site;
+	private int view_count;
 }
