19dbd53b9df1e19f0673c465804f8ff015cb8031
dbwldk <yujiayujia55@naver.com>
2024-05-29 16:54:45 +0900

    id로 데이터 받아오기
    - id로 데이터 받아오기
    - 날짜 데이터 포맷

--- Project/TOW_Backend/src/main/java/com/tow/controller/MainController.java
+++ Project/TOW_Backend/src/main/java/com/tow/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.tow.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tow.domain.TicketDB;
 import com.tow.service.MainService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -19,5 +22,11 @@ public class MainController {
 	public List<TicketDB> main() {
 		return mainService.getAll();
 	}
+	
+	@GetMapping("one")
+	public Optional<TicketDB> getMethodName(@RequestParam int id) {
+		return mainService.getOne(id);
+	}
+	
 	
 }
