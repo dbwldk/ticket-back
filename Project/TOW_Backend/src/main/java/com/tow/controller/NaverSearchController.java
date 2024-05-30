45952e10a66c8c83eab31b07301d69bff1b79410
dbwldk <yujiayujia55@naver.com>
2024-05-31 01:55:29 +0900

    환경변수 설정
    .env 파일 /위치에 추가해서 쓰면 됨

--- Project/TOW_Backend/src/main/java/com/tow/controller/NaverSearchController.java
+++ Project/TOW_Backend/src/main/java/com/tow/controller/NaverSearchController.java
@@ -13,19 +13,26 @@ import com.tow.domain.vo.NaverResultVO;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
 @RestController
 public class NaverSearchController {
+	
+	@Value("${NAVER_SEARCH_CLIENT_ID}")
+	private String NaverClientId;
+	
+	@Value("${NAVER_SEARCH_CLIENT_SECRET}")
+	private String NaverClientSecret;
+	
 	@GetMapping("naverSearch")
 	public List<LocalVO> getMethodName(@RequestParam String searchPlace) {
+		//uri 설정
 		URI uri = UriComponentsBuilder
 				.fromUriString("https://openapi.naver.com")
 				.path("/v1/search/local.json")
@@ -37,14 +44,16 @@ public class NaverSearchController {
                 .build()
                 .toUri();
 		
+		//request & response
 		RestTemplate restTemplate = new RestTemplate();		
 		RequestEntity<Void> req = RequestEntity
 				.get(uri)
-				.header("X-Naver-Client-Id", "") //나중에 환경변수 설정
-				.header("X-Naver-Client-Secret", "") //나중에 환경변수 설정
+				.header("X-Naver-Client-Id", NaverClientId)
+				.header("X-Naver-Client-Secret", NaverClientSecret)
 				.build();
 		ResponseEntity<String> response = restTemplate.exchange(req, String.class);
 		
+		//vo에 넣어서 json으로 내보내기
 		ObjectMapper om = new ObjectMapper();
 		NaverResultVO resultVO = null;
 		
