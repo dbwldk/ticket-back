cac4bfdee835f3f35d9e67bcff61ca2736159e53
김민준 <hanleejss012@naver.com>
2024-06-10 19:44:55 +0900

    MainPost

---
+++ Project/TOW_Backend/src/main/java/com/tow/repository/MainPostRepositroy.java
@@ -0,0 +1,21 @@
+package com.tow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.tow.domain.MainPostTicketDB;
+
+import java.util.List;
+
+@Repository
+public interface MainPostRepositroy extends JpaRepository<MainPostTicketDB, Integer> {
+	//인기 Top 10
+	 @Query(value = "SELECT * FROM tickets ORDER BY id DESC LIMIT 10", nativeQuery = true)
+	List<MainPostTicketDB> findTop10();
+	
+	//Month 월별 id순으로 limit
+	 @Query(value = "SELECT * FROM tickets WHERE MONTH(ticket_open_date) = :month AND YEAR(ticket_open_date) = :year ORDER BY id DESC LIMIT :limit", nativeQuery = true)
+	 List<MainPostTicketDB> findTicketsForCurrentMonth(@Param("month") int month, @Param("year") int year, @Param("limit") int limit);
+}
