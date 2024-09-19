01523474755722513fc9ebd0f93af0a795cf7286
dbwldk <yujiayujia55@naver.com>
2024-09-20 02:37:50 +0900

    검색 백엔드 1차 테스트
    - 장르, 지역, 검색어, 관람 기간 필터링 로직 완료
    > 지역, 검색어 필터는 테스트도 완료
    
    - db에 region 항목 및 트리거 추가함
    
    - 나머지 필터링도 테스트 완료되면, 정렬도 추가 예정(인기순(관심순), 조회순)
    > 조회수랑 관심수는 테이블 분리해서 쓰는게 나을듯

---
+++ Project/TOW_Backend/src/main/java/com/tow/domain/SearchTicketDB.java
@@ -0,0 +1,32 @@
+package com.tow.domain;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tickets")
+public class SearchTicketDB {
+    @Id
+    private Integer id;
+    private Timestamp ticket_open_date;
+    private Timestamp pre_sale_date;
+    private String image_url;
+    // 필터링
+    private String genre; //장르 필터링
+    private String region; // 지역 필터링
+    private String event_name; // 검색어 필터링
+    private Date event_start_date; // 관람 기간 필터링
+    private Date event_end_date;
+    
+    @OneToMany(mappedBy = "ticketDB", fetch = FetchType.EAGER)
+    private List<EventSiteDB> eventSites;
+}
---
+++ Project/TOW_Backend/src/main/java/com/tow/repository/SearchTicketRepository.java
@@ -0,0 +1,26 @@
+package com.tow.repository;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.tow.domain.SearchTicketDB;
+
+@Repository
+public interface SearchTicketRepository extends JpaRepository<SearchTicketDB, Integer> {
+	@Query("SELECT s FROM SearchTicketDB s " +
+			"WHERE (:regionList IS NULL OR s.region IN :regionList) " +
+			"AND (:genreList IS NULL OR s.genre IN :genreList) " +
+			"AND (:startDate IS NULL OR :endDate IS NULL OR (s.event_start_date <= :endDate AND s.event_end_date >= :startDate)) "+
+			"AND (:searchKeyword IS NULL OR s.event_name LIKE %:searchKeyword%)")
+	List<SearchTicketDB> findByFilters(
+			@Param("regionList") List<String> regionList,
+            @Param("genreList") List<String> genreList,
+            @Param("startDate") Date startDate,
+            @Param("endDate") Date endDate,
+            @Param("searchKeyword") String searchKeyword);
+}
