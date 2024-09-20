6e7c16b8f5bbd69afc26c1b4df3b97889877e938
dbwldk <yujiayujia55@naver.com>
2024-09-21 02:40:43 +0900

    검색 페이지 백엔드 완료
    - 자동 완성(서버 교체 후)과 정렬(로그인, 좋아요 진행 후)을 제외한 검색 백엔드 완성

--- Project/TOW_Backend/src/main/java/com/tow/service/SearchService.java
+++ Project/TOW_Backend/src/main/java/com/tow/service/SearchService.java
@@ -6,6 +6,8 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.tow.domain.SearchTicketDB;
@@ -22,8 +24,9 @@ public class SearchService {
 		return new java.sql.Date(parseDate.getTime());
 	}
 	
-	public List<SearchTicketDB> testSearchResult(List<String> regionList, List<String> genreList,
-			Date startDate, Date endDate, String searchKeyword) {
-		return sticketRep.findByFilters(regionList, genreList, startDate, endDate, searchKeyword);
+	public Page<SearchTicketDB> findPageByFilters(List<String> regionList, List<String> genreList,
+			Date startDate, Date endDate, String searchKeyword,
+			Pageable pageable) {
+		return sticketRep.findPageByFilters(regionList, genreList, startDate, endDate, searchKeyword, pageable);
 	}
 }
