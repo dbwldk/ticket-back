ede1e2b1d5229c2d376289b4fab5917252d31703
dbwldk <yujiayujia55@naver.com>
2024-11-08 03:50:15 +0900

    네이버 지도 코드 수정

--- Project/TOW_Backend/src/main/java/com/tow/controller/NaverSearchController.java
+++ Project/TOW_Backend/src/main/java/com/tow/controller/NaverSearchController.java
@@ -6,15 +6,21 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tow.domain.vo.LocalVO;
 import com.tow.domain.vo.NaverResultVO;
 
 import java.net.URI;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,15 +29,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class NaverSearchController {
-	
+	// 환경 변수 선언
 	@Value("${NAVER_SEARCH_CLIENT_ID}")
 	private String NaverClientId;
 	
 	@Value("${NAVER_SEARCH_CLIENT_SECRET}")
 	private String NaverClientSecret;
 	
-	@GetMapping("naverSearch")
-	public List<LocalVO> getMethodName(@RequestParam String searchPlace) {
+	@Value("${NAVER_CLOUD_ID}")
+	private String NaverCloudId;
+	
+	@Value("${NAVER_CLOUD_SECRET}")
+	private String NaverCloudSecret;
+	
+	// 장소 검색 naver search
+	@GetMapping("naverPlaceSearch")
+	public List<LocalVO> getGeocodeFromPlace(@RequestParam String searchPlace) {
 		//uri 설정
 		URI uri = UriComponentsBuilder
 				.fromUriString("https://openapi.naver.com")
@@ -68,5 +81,42 @@ public class NaverSearchController {
         return resultVO != null ? resultVO.getItems() : null;
 	}
 	
-	
+	// 주소 검색 naver map
+	@GetMapping("naverAddrSearch")
+	public Map<String, String> getGeocodeFromAddr(@RequestParam String searchAddr) {
+		// 선언
+		RestTemplate restTemplate = new RestTemplate();
+		ObjectMapper om = new ObjectMapper();
+		
+		// url 설정
+		String apiUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + searchAddr;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-ncp-apigw-api-key-id", NaverCloudId);
+        headers.set("x-ncp-apigw-api-key", NaverCloudSecret);
+        headers.set("Accept", "application/json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+
+        // response > JSON : geocode 부분만 가져오기
+        try {
+        	JsonNode root = om.readTree(response.getBody());
+            JsonNode addressNode = root.path("addresses").get(0);
+
+            String x = addressNode.path("x").asText();
+            String y = addressNode.path("y").asText();
+
+            Map<String, String> coordinates = new HashMap<>();
+            coordinates.put("x", x);
+            coordinates.put("y", y);
+
+            return coordinates;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+    
 }
