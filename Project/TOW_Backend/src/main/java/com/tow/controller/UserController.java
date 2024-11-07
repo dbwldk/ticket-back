4485561533d987de948de456f8dd72a0a57350a0
dbwldk <yujiayujia55@naver.com>
2024-11-07 19:17:02 +0900

    로그아웃

--- Project/TOW_Backend/src/main/java/com/tow/controller/UserController.java
+++ Project/TOW_Backend/src/main/java/com/tow/controller/UserController.java
@@ -4,38 +4,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tow.domain.NaverUserDB;
-import com.tow.domain.vo.UpdatePasswordRequestVO;
 import com.tow.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class UserController {
+    
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
     public NaverUserDB register(@RequestBody NaverUserDB user) {
-    	
         return userService.registerUser(user);
     }
     
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody NaverUserDB user, HttpSession session) {
+    public ResponseEntity<NaverUserDB> login(@RequestBody NaverUserDB user, HttpSession session) {
         boolean isAuthenticated = userService.authenticateUser(user.getEmail(), user.getPassword());
         if (isAuthenticated) {
-        	// 세션에 사용자 정보를 저장
-        	session.setAttribute("user", user);
-            return ResponseEntity.ok("로그인 성공");
+            // 세션에 사용자 정보를 저장
+            session.setAttribute("user", user);
+            return ResponseEntity.ok(user); // NaverUserDB 객체를 반환
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // 인증 실패
         }
     }
+    
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        session.invalidate(); // 세션 무효화
+        return ResponseEntity.ok("로그아웃 성공");
+    }
+}
+
+//package com.tow.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.tow.domain.NaverUserDB;
+//import com.tow.domain.vo.UpdatePasswordRequestVO;
+//import com.tow.service.UserService;
+//
+//import jakarta.servlet.http.HttpSession;
+//
+//@RestController
+//public class UserController {
+//    @Autowired
+//    private UserService userService;
+//
+//    @PostMapping("/register")
+//    public NaverUserDB register(@RequestBody NaverUserDB user) {
+//       
+//        return userService.registerUser(user);
+//    }
+//    
+//    @PostMapping("/login")
+//    public ResponseEntity<String> login(@RequestBody NaverUserDB user, HttpSession session) {
+//        boolean isAuthenticated = userService.authenticateUser(user.getEmail(), user.getPassword());
+//        if (isAuthenticated) {
+//           // 세션에 사용자 정보를 저장
+//           session.setAttribute("user", user);
+//            return ResponseEntity.ok("");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
+//        }
+//    }
+//    
+//    @PostMapping("/logout")
+//    public ResponseEntity<String> logout(HttpSession session) {
+//        session.invalidate(); // 세션 무효화
+//        return ResponseEntity.ok("로그아웃 성공");
+//    }
     
 //    @PutMapping("/update-password")
 //    public ResponseEntity<String> updatePassword(@RequestBody UpdatePasswordRequestVO request, HttpSession session) {
@@ -53,4 +103,4 @@ public class UserController {
 //        }
 //    }
     
-}
+//}
