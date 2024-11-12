1311074bcbfac40e99a31a86597042520043a6e4
dbwldk <yujiayujia55@naver.com>
2024-11-13 01:37:04 +0900

    마이페이지 목록 완료

--- Project/TOW_Backend/src/main/java/com/tow/repository/ReservationRepository.java
+++ Project/TOW_Backend/src/main/java/com/tow/repository/ReservationRepository.java
@@ -1,6 +1,9 @@
 package com.tow.repository;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,10 +23,21 @@ public interface ReservationRepository extends JpaRepository<ReservationDB, Inte
 	@Query("SELECT COUNT(r) FROM ReservationDB r WHERE r.ticketId = :tId AND r.email = :uId")
 	long countByTidAndUid(@Param("tId") Integer tId, @Param("uId") String uId);
 	
-	// 사용자가 알림 누른 모든 티켓 가져오기
+	// 사용자가 알림 누른 모든 티켓 가져오기: list
 	List<ReservationDB> findByEmailOrderByReservationTimeDesc(@Param("email") String email);
+	
+	// 사용자가 알림 누른 모든 티켓 가져오기: page
+	Page<ReservationDB> findByEmailOrderByReservationTimeDesc(@Param("email") String email, Pageable pageable);
 	
 	// 사용자가 알림 누른 티켓 수 가져오기
 	@Query("SELECT COUNT(r) FROM ReservationDB r WHERE r.email = :userId")
 	long countByUserId(@Param("userId") String userId);
+	
+	// 티켓 알림 시간 가져오기
+	@Query("SELECT r.notificationHours FROM ReservationDB r WHERE r.ticketId = :tId AND r.email = :uId")
+	int findNotificationHours(@Param("tId") Integer tId, @Param("uId") String uId);
+	
+	// 티켓 업데이트를 위한 쿼리
+	@Query("SELECT r FROM ReservationDB r WHERE r.ticketId = :tId AND r.email = :uId")
+	Optional<ReservationDB> findByTidAndUid(@Param("tId") Integer tId, @Param("uId") String uId);
 }
